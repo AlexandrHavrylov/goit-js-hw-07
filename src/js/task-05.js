f59c2322bcbaa8dcfa77e1e8fcd118f8bc06c568
Sasha Havrylov <sashagavrilov1995@gmail.com>
2021-06-02 22:13:21 +0300

    решение 5 задачи

---
+++ src/js/task-05.js
@@ -0,0 +1,13 @@
+const nameInputEl = document.querySelector('#name-input')
+const nameOutputEl = document.querySelector('#name-output')
+
+nameInputEl.addEventListener('input', event => {
+
+    nameOutputEl.textContent = 'незнакомец'
+    if (event.currentTarget.value !== "") {
+          nameOutputEl.textContent = event.currentTarget.value
+    }
+   
+   
+    
+})
